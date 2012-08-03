643a705a50b3f6918b2aa367be3557130b2a9c74
Makis Tracend <makis.tracend@gmail.com>
2012-08-03 03:37:36 -0700

    Updating default scope (for facebook) to include images

--- bin/config.php
+++ bin/config.php
@@ -10,7 +10,7 @@ if( class_exists('Config') && method_exists(new Config(),'register')){
 	// Register variables
 	Config::register("singly", "key", "01234567890");
 	Config::register("singly", "secret", "012345678901234567890123456789");
-	Config::register("singly", "facebook_scope", "user_about_me");
+	Config::register("singly", "facebook_scope", "user_about_me,user_photos");
 	
 }
 

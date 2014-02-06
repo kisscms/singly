a62e8ed0e92a56f188eacaa44bd9397055b8e356
Makis Tracend <makis.tracend@gmail.com>
2014-02-05 19:07:44 -0800

    setting api name

--- helpers/singly.php
+++ helpers/singly.php
@@ -8,45 +8,46 @@
 
 class Singly extends Remote_API {
 
+	public $name = "singly";
 	//public $singly;
 	private  $config;
 	private  $creds;
 	public $oauth;
-	
+
 	// initialize
-	function __construct(){ 
-		
+	function __construct(){
+
 		$this->url = "https://api.singly.com/v0/";
-		
+
 		$this->config = $GLOBALS['config']['singly'];
-		
+
 		$this->init();
-		
+
 	}
-	
+
 	function init(){
 		// load all the necessery subclasses
 		$this->oauth = new Singly_OAuth();
 		$this->login();
 	}
-	
+
 	function login(){
-		
+
 		// get/update the creds
 		$this->creds = $this->oauth->creds();
-		
+
 		// check if the credentials are empty
 		return !empty($this->creds);
-	
+
 	}
-	
+
 	function me(){
-		
+
 		// get the connected services
 		$me = $this->get("profiles");
-		
+
 		return $me;
-		
+
 		/*// loop through all the available services
 		foreach( $me as $name=>$profile ){
 			// leave the global id singly creates alone...
@@ -59,76 +60,76 @@ class Singly extends Remote_API {
 			} else {
 				$me[$name] = $this->get("services/". $name ."/self");
 			}
-			
+
 		}*/
-		
+
 	}
-	
-	
+
+
 	function get( $service="", $params=array() ){
-		
+
 		// check cache before....
-		
+
 		// add access_token
 		if( empty($params['access_token']) ) $params['access_token'] = $this->creds['access_token'];
-		
+
 		$http = new Http();
 		$http->setMethod('GET');
 		$http->setParams($params);
-		
+
 		$http->execute( $this->url . $service );
-		
+
 		// add here validation/conditions for result...
 		$results = json_decode($http->result, true);
-		
+
 		// cache result
 		//$this->setCache( $service, $params, $results );
-		
+
 		return $results;
-		
+
 	}
-	
+
 	function post( $service="", $params=array() ){
-		
+
 		// check cache before....
-		
+
 		// add access_token
 		if( empty($params['access_token']) ) $params['access_token'] = $this->creds['access_token'];
-		
+
 		$http = new Http();
 		$http->setMethod('POST');
 		$http->setParams( $params );
-		
+
 		$http->execute( $this->url . $service );
-		
+
 		// add here validation/conditions for result...
 		$results = json_decode($http->result, true);
-		
+
 		// cache result
 		//$this->setCache( $service, $params, $results );
-		
+
 		return $results;
-		
+
 	}
-	
+
 	function delete(){
-		
+
 	}
-	
+
 	function logout( $params=array() ){
-		
+
 		// add access_token
 		if( empty($params['access_token']) ) $params['access_token'] = $this->creds['access_token'];
 		// add redirect_uri ?
-		
+
 		$http = new Http();
 		$http->setMethod('GET');
 		$http->setParams($params);
 		// execute request
 		$http->execute( "https://api.singly.com/logout" );
-		
+
 	}
-	
+
 }
 
 
